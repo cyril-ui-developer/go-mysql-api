043298ce3d3a7365bfc0ebf4980b21e0b55f830d
CYRIL AJIEH <cyril.ajieh@gmail.com>
2021-11-02 14:02:52 -0400

    add array of json objects

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 	"github.com/go-chi/chi"
 )
 
+type Employee struct {
+	ID        int
+	FirstName string `json: "firstName`
+	LastName string `json: "lastName"`
+	Address   string `json: "address"`
+}
+
 func jsonRReesponse(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	b, err := json.Marshal(data)
@@ -18,7 +25,6 @@ func jsonRReesponse(w http.ResponseWriter, data interface{}) {
 		jsonRReesponse(w, map[string]string{"error": err.Error()})
 		return
 	}
-
 	w.Write(b)
 }
 
@@ -34,10 +40,23 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to Go and MySQL API"))
 	})
-	r.Get("/employees", func(w http.ResponseWriter, r *http.Request){
-		jsonRReesponse(w, map[string]string{"name": "Cyril"})
+	r.Get("/employees", func(w http.ResponseWriter, r *http.Request) {
+		employees := []Employee{
+			Employee{
+				LastName: "Cyril", FirstName: "Ajieh",
+			},
+			Employee{
+				LastName: "Campbell", FirstName: "David",
+			},
+			Employee{
+				LastName: "Doe", FirstName: "John",
+			},
+		}
+		// jsonRReesponse(w, map[string]string{"name": "Cyril"})
+		jsonRReesponse(w, employees)
 	})
 	fmt.Println("Welcome to Go and MySQL API")
 
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
+
